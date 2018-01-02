81e377845c7766dc38d48f120112b0aa2f117f87
MuQi <765879176@qq.com>
2018-01-02 17:53:58 +0800

    第二次提交

---
+++ Rxjava/src/main/java/com/mu/example/myapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.mu.example.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        method7();
+    }
+
+    private void method1() {
+        Flowable.just("Hello world").subscribe(System.out::println);
+
+    }
+
+    private void method2() {
+        Flowable.fromCallable(() -> {
+            Thread.sleep(1000); //  imitate expensive computation
+            return "Done";
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.single())
+                .subscribe(System.out::println, Throwable::printStackTrace);
+
+        try {
+            Thread.sleep(2000); // <--- wait for the flow to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void method3() {
+        Flowable<String> source = Flowable.fromCallable(() -> {
+            Thread.sleep(1000); //  imitate expensive computation
+            return "Done";
+        });
+
+        Flowable<String> runBackground = source.subscribeOn(Schedulers.io());
+
+        Flowable<String> showForeground = runBackground.observeOn(Schedulers.single());
+
+        showForeground.subscribe(System.out::println, Throwable::printStackTrace);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void method4() {
+        Flowable.range(1, 10)
+                .observeOn(Schedulers.computation())
+                .map(v -> v * v)
+                .blockingSubscribe(System.out::println);
+    }
+
+    private void method5() {
+        Flowable.range(1, 10)
+                .flatMap(v ->
+                        Flowable.just(v)
+                                .subscribeOn(Schedulers.computation())
+                                .map(w -> w * w)
+                )
+                .blockingSubscribe(System.out::println);
+    }
+
+    private void method6() {
+        Flowable.range(1, 10)
+                .parallel()
+                .runOn(Schedulers.computation())
+                .map(v -> v * v)
+                .sequential()
+                .blockingSubscribe(System.out::println);
+    }
+    private void method7() {
+        Observable.just("one", "two", "three", "four", "five")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(System.out::println);
+    }
+
+
+}
