538bb169650b3176a5d25b9dc2f517641a05a0b0
MumuGit <765879176@qq.com>
2018-04-25 17:32:45 +0800

    sdff

--- ApplyFineGestureDector/src/main/java/com/mu/example/applyfinegesturedector/ViewDragActivity.java
+++ ApplyFineGestureDector/src/main/java/com/mu/example/applyfinegesturedector/ViewDragActivity.java
@@ -3,9 +3,8 @@ package com.mu.example.applyfinegesturedector;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class ViewDragActivity extends AppCompatActivity {
@@ -17,13 +16,13 @@ public class ViewDragActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_drag);
-//        button = (android.widget.Button) findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                System.out.println("sss");
-//            }
-//        });
+        button = (android.widget.Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText()
+            }
+        });
 //        button.setOnLongClickListener(new View.OnLongClickListener() {
 //            @Override
 //            public boolean onLongClick(View v) {
